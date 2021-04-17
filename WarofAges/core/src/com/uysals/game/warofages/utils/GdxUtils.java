a62ab2d06cfe4b87099a1108f290ae6dd722b8ea
uysalGokhan <uysal.gokhan.2017@gmail.com>
2021-04-17 18:36:11 +0300

    Game screen sizes are adjusted

---
+++ WarofAges/core/src/com/uysals/game/warofages/utils/GdxUtils.java
@@ -0,0 +1,20 @@
+package com.uysals.game.warofages.utils;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+
+public class GdxUtils {
+
+    public static void clearScreen() {
+        clearScreen(Color.WHITE);
+    }
+
+    public static void clearScreen(Color color) {
+        // clear screen
+        // DRY - Don't repeat yourself
+        // WET - Waste everyone's time
+        Gdx.gl.glClearColor(color.r, color.g, color.b, color.a);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+}
