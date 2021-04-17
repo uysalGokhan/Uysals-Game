d1b17e026bae0777c01770004a85f1aa7329a9bd
OkanUysal <uysal.okan.2010@gmail.com>
2021-04-17 17:49:26 +0300

    create screen layer

--- WarofAges/core/src/com/uysals/game/warofages/WarofAges.java
+++ WarofAges/core/src/com/uysals/game/warofages/WarofAges.java
@@ -1,33 +1,57 @@
 package com.uysals.game.warofages;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Logger;
 
-public class WarofAges extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class WarofAges extends Game {
+
+	private AssetManager assetManager;
+	private SpriteBatch batch;
+	private Preferences prefs;
+	private int changeMenuCount;
+
 	@Override
-	public void create () {
+	public void create() {
+		Gdx.app.setLogLevel(Application.LOG_DEBUG);
+		changeMenuCount = 0;
+		assetManager = new AssetManager();
+		assetManager.getLogger().setLevel(Logger.DEBUG);
+		prefs = Gdx.app.getPreferences("WarofAgesData");
+
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		//newScreen(new LoadingScreen(this));
+	}
+
+	public void newScreen(Screen screen) {
+		changeMenuCount++;
+		setScreen(screen);
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
+	public void dispose() {
+		assetManager.dispose();
 		batch.dispose();
-		img.dispose();
+	}
+
+	public AssetManager getAssetManager() {
+		return assetManager;
+	}
+
+	public SpriteBatch getBatch() {
+		return batch;
+	}
+
+	public Preferences getPreferences() {
+		return prefs;
 	}
 }
