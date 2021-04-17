bbc8866c5729c8e8aa3b0d3dea30f3cb016a582f
OkanUysal <uysal.okan.2010@gmail.com>
2021-04-17 23:12:02 +0300

    assetpacker project is created

---
+++ WarofAges/desktop/src/com/uysals/game/warofages/desktop/AssetPacker.java
@@ -0,0 +1,33 @@
+package com.uysals.game.warofages.desktop;
+
+import java.io.File;
+
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+
+public class AssetPacker {
+
+    private static final boolean DRAW_DEBUG_OUTLINE = false;
+
+    private static final String RAW_ASSETS_PATH = "assets_raw";
+    private static final String ASSETS_PATH = "assets";
+
+
+    public static void main(String[] args) {
+
+        TexturePacker.Settings settings = new TexturePacker.Settings();
+        settings.debug = DRAW_DEBUG_OUTLINE;
+        settings.maxHeight = 2048;
+        settings.maxWidth = 2048;
+
+        new File(ASSETS_PATH + "/gameBasic").mkdirs();
+
+        TexturePacker.process(settings,
+                RAW_ASSETS_PATH + "",
+                ASSETS_PATH + "/gameBasic",
+                "floor");
+
+
+
+
+    }
+}
