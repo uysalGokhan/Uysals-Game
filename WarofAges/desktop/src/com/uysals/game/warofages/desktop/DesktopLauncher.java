7c1f2705aedbcc053c167fdd37e47375751843ed
OkanUysal <uysal.okan.2010@gmail.com>
2021-04-17 12:53:44 +0300

    Creating Libgdx Project

---
+++ WarofAges/desktop/src/com/uysals/game/warofages/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.uysals.game.warofages.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.uysals.game.warofages.WarofAges;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new WarofAges(), config);
+	}
+}
