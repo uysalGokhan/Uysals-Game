5eb218bf308fa723f6cf7236b570db11dd642368
OkanUysal <uysal.okan.2010@gmail.com>
2021-04-24 12:00:11 +0300

    add map generator

---
+++ WarofAges/desktop/src/com/uysals/game/warofages/desktop/CreateDefaultMap.java
@@ -0,0 +1,67 @@
+package com.uysals.game.warofages.desktop;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.uysals.game.warofages.GameConfig;
+import com.uysals.game.warofages.objects.Floor;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class CreateDefaultMap {
+
+    public static int floorCount = 19;
+    public static int rowCount = 11; // always odd number
+    public static int columnCount = 11; // always odd number
+
+    public static ArrayList<Floor> floors = new ArrayList<Floor>();
+    public static Random random = new Random();
+
+    public static void main(String[] args) {
+        float xCoor;
+        float yCoor;
+        int disable;
+        int region;
+        // rowCount / 2 + 1 = HEIGHT / 2 = 540f;
+        // columnCount / 2 + 1 = WIDTH / 2 = 960f;
+        for(int i = 0; i < rowCount; i++) {
+            for(int j = 0; j < rowCount; j++) {
+                xCoor = GameConfig.WORLD_CENTER_X + ((j - (columnCount / 2 + 1)) * 198f);
+                if(i % 2 == 0)
+                    xCoor -= 100f;
+                yCoor = GameConfig.WORLD_CENTER_Y - ((i - (rowCount / 2 + 1)) * 180f);
+                if(i == rowCount / 2 + 1 && j == columnCount / 2 + 1) {
+                    region = 0;
+                } else {
+                    region = random.nextInt(floorCount) + 1;
+                }
+                if(i >= rowCount / 2 - 1 && i <= rowCount / 2 + 3 && j >= columnCount / 2 - 1 && j < columnCount / 2 + 3) {
+                    if(i >= rowCount / 2  && i <= rowCount / 2 + 2 && j >= columnCount / 2  && j < columnCount / 2 + 2) {
+                        disable = 0;
+                    } else {
+                        disable = 1;
+                    }
+                } else {
+                    disable = 2;
+                }
+
+                floors.add(new Floor(xCoor,yCoor,disable,region));
+            }
+        }
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(new File("defaultMap.json")));
+            writer.write(gson.toJson(floors));
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
